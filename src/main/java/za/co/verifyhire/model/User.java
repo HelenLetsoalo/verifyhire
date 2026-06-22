132e4da44fe1ea4557f2cda2f36d83c47f07f1e7
Danielle Hoffman <dahofjhb025@student.wethinkcode.co.za>
2026-06-22 14:39:07 +0200

    feat: add User entity class

---
+++ src/main/java/za/co/verifyhire/model/User.java
@@ -0,0 +1,41 @@
+public class User{
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String location;
+
+    public User(String userId, String firstName, String lastName, String email,String password,String location) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.location = location;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
