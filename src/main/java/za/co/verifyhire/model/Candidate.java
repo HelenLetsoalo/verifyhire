e20aa2aa31944cbb2750de925ade00e49c08f2a6
Danielle Hoffman <dahofjhb025@student.wethinkcode.co.za>
2026-06-22 14:43:43 +0200

    feat: add Candidate entity class

---
+++ src/main/java/za/co/verifyhire/model/Candidate.java
@@ -0,0 +1,27 @@
+public class Candidate extends User{
+
+    private Qualification qualification;
+    private WorkExperience experience;
+    private Reference reference;
+
+    public Candidate(String userId, String firstName, String lastName, String email,String password,String location, Qualification qualification, WorkExperience experience,Reference reference) {
+        super(userId, firstName, lastName,email,password,location);
+        this.qualification = qualification;
+        this.experience = experience;
+        this.reference = reference;
+    }
+
+    // Getters
+
+    public Qualification getQualification() {
+        return qualification;
+    }
+
+    public WorkExperience getExperience() {
+        return experience;
+    }
+
+    public Reference getReference() {
+        return reference;
+    }
+}
