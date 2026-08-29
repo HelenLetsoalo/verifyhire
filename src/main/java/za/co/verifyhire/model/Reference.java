5150a943b5d3a69304909ca70924652d52fba0bd
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-29 19:14:33 +0200

    feat: add constructor for Reference

---
+++ src/main/java/za/co/verifyhire/model/Reference.java
@@ -0,0 +1,18 @@
+public class Reference {
+
+    private String referenceId;
+    private String name;
+    private String relationship;
+    private String email;
+    private String phoneNumber;
+
+    public Reference(String referenceId, String name, String relationship, String email, String phoneNumber){
+        this.referenceId = referenceId;
+        this.name = name;
+        this.relationship = relationship;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+}
