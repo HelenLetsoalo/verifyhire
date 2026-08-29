9232da92787235e13fd41483c8ca351fde210561
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-29 19:46:08 +0200

    feat: add recruiter constructor

---
+++ src/main/java/za/co/verifyhire/model/Recruiter.java
@@ -0,0 +1,8 @@
+public class Recruiter extends User{
+
+
+    public Recruiter(String userId, String firstName, String lastName, String email, String password, String location){
+        super(userId,firstName,lastName,email,password,location)
+    }
+
+}
