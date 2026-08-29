0b8f2b75506acb69089af2a55894018b3e96d1b3
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-29 16:24:05 +0200

    fix: fix String qualificationID to qualificationId. A more conventional Java camelCase style

--- src/main/java/za/co/verifyhire/model/Qualification.java
+++ src/main/java/za/co/verifyhire/model/Qualification.java
@@ -1,12 +1,12 @@
 public class Qualification {
 
-    private String qualificationID;
+    private String qualificationId;
     private String name;
     private String institutionName;
     private int yearObtained;
 
-    public Qualification (String qualificationID, String name, String institutionName, int yearObtained) {
-        this.qualificationID = qualificationID;
+    public Qualification (String qualificationId, String name, String institutionName, int yearObtained) {
+        this.qualificationId = qualificationId;
         this.name = name;
         this.institutionName = institutionName;
         this.yearObtained = yearObtained;
