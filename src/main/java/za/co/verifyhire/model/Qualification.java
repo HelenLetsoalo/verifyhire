5403f8e848ae4db52ef876907248aaaeaf22648a
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-29 16:21:38 +0200

    feat: add qualification constructor

---
+++ src/main/java/za/co/verifyhire/model/Qualification.java
@@ -0,0 +1,15 @@
+public class Qualification {
+
+    private String qualificationID;
+    private String name;
+    private String institutionName;
+    private int yearObtained;
+
+    public Qualification (String qualificationID, String name, String institutionName, int yearObtained) {
+        this.qualificationID = qualificationID;
+        this.name = name;
+        this.institutionName = institutionName;
+        this.yearObtained = yearObtained;
+    }
+
+}
