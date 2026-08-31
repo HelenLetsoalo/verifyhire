88a9cbf5c49a01d7b6349b1ab7723b435f1aa9c4
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-31 08:46:26 +0200

    feat: add inherited fields from User and add extra fields for Recruiter

--- src/main/java/za/co/verifyhire/model/Recruiter.java
+++ src/main/java/za/co/verifyhire/model/Recruiter.java
@@ -1,8 +1,12 @@
 public class Recruiter extends User{
 
+    private String companyName;
+    private boolean isVerified;
 
-    public Recruiter(String userId, String firstName, String lastName, String email, String password, String location){
-        super(userId,firstName,lastName,email,password,location)
+    public Recruiter(String userId, String firstName, String lastName, String email, String password, String location, String companyName, boolean isVerified){
+        super(userId,firstName,lastName,email,password,location);
+        this.companyName = companyName;
+        this.isVerified = false;
     }
 
 }
