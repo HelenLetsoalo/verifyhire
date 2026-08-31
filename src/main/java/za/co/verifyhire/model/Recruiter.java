7fb39e7ff507a34ad3c1c32b577dfe9cc5af6f4f
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-08-31 08:48:08 +0200

    fix: initialize recruiter as unverified

--- src/main/java/za/co/verifyhire/model/Recruiter.java
+++ src/main/java/za/co/verifyhire/model/Recruiter.java
@@ -3,7 +3,7 @@ public class Recruiter extends User{
     private String companyName;
     private boolean isVerified;
 
-    public Recruiter(String userId, String firstName, String lastName, String email, String password, String location, String companyName, boolean isVerified){
+    public Recruiter(String userId, String firstName, String lastName, String email, String password, String location, String companyName){
         super(userId,firstName,lastName,email,password,location);
         this.companyName = companyName;
         this.isVerified = false;
