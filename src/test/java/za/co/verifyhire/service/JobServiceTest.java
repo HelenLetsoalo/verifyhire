a54112526d9360e4bbe2fc7991eaf39d236ab300
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-11 00:32:39 +0200

    test: add job registration

--- src/test/java/za/co/verifyhire/service/JobServiceTest.java
+++ src/test/java/za/co/verifyhire/service/JobServiceTest.java
@@ -4,4 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import za.co.verifyhire.model.Job;
 
-public class JobServiceTest {}
+public class JobServiceTest {
+
+    @Test
+    public void shouldAddJob(){
+        JobService service = new JobService;
+
+        Job job = new Job(
+                "J1",
+                "Software Developer",
+                "Develop and maintain software.",
+                "Johannesburg",
+                "Java, Python
+        );
+        service.addJob(job);
+
+        assertEquals(job, service.findJobById());
+    }
+}
