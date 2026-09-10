72a1bfb8dc7fb11ef9fa6959cf5f84880953142e
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:02:18 +0200

    feat: Add appeal ID getter test

--- src/test/java/za/co/verifyhire/model/AppealTest.java
+++ src/test/java/za/co/verifyhire/model/AppealTest.java
@@ -6,5 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppealTest {
 
+    @Test
+    public void shouldReturnAppealId() {
+        Appeal appeal = new Appeal(
+                "A1",
+                null,
+                "I believe my qualification was incorrectly rejected."
+        );
 
+        assertEquals("A1", appeal.getAppealId());
+    }
 }
