fd9ffe858899ac915f138e3a314a175cffc8317a
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:09:13 +0200

    feat: Add attributes in appeal class

--- src/main/java/za/co/verifyhire/model/Appeal.java
+++ src/main/java/za/co/verifyhire/model/Appeal.java
@@ -2,4 +2,9 @@ package za.co.verifyhire.model;
 
 public class Appeal {
 
+    private String appealId;
+    private JobApplication application;
+    private String reason;
+    private AppealStatus status;
+
 }
