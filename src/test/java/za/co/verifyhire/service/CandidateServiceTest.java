c2a7056d5481396b746a29fc869e80f98b25e8df
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-11 00:14:48 +0200

    test: return null for unknown candidate

--- src/test/java/za/co/verifyhire/service/CandidateServiceTest.java
+++ src/test/java/za/co/verifyhire/service/CandidateServiceTest.java
@@ -2,6 +2,33 @@ package za.co.verifyhire.service;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import za.co.verifyhire.model.User;
+import za.co.verifyhire.model.Candidate;
 
-public class CandidateServiceTest {}
+public class CandidateServiceTest {
+    @Test
+    public void shouldAddCandidate() {
+        CandidateService service = new CandidateService();
+
+        Candidate candidate = new Candidate(
+                "C1",
+                "Helen",
+                "Letsoalo",
+                "helen@example.com",
+                "password",
+                "Johannesburg"
+        );
+
+        service.addCandidate(candidate);
+
+        assertEquals(candidate, service.findCandidateById("C1"));
+    }
+
+    @Test
+    public void shouldReturnNullWhenCandidateDoesNotExist() {
+        CandidateService service = new CandidateService();
+
+        Candidate foundCandidate = service.findCandidateById("C999");
+
+        assertEquals(null, foundCandidate);
+    }
+}
