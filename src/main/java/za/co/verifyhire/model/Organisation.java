f090001b64bcbefc6acb4f0ffe6d288289bf0326
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 20:54:04 +0200

    feat: Add organisation attributes

--- src/main/java/za/co/verifyhire/model/Organisation.java
+++ src/main/java/za/co/verifyhire/model/Organisation.java
@@ -1,1 +1,13 @@
 package za.co.verifyhire.model;
+
+public class Organisation {
+
+    private String organisationId;
+    private String name;
+    private String location;
+    private String email;
+    private String services;
+    private String phone;
+
+
+}
