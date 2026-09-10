7d12eb721ee9cc3cb90d80aea5d5bd12765f8e53
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:50:14 +0200

    feat: Add private attributes

--- src/main/java/za/co/verifyhire/model/JobRecruitment.java
+++ src/main/java/za/co/verifyhire/model/JobRecruitment.java
@@ -2,4 +2,9 @@ package za.co.verifyhire.model;
 
 public class JobRecruitment {
 
+    private String recruitmentId;
+    private Job job;
+    private Organisation organisation;
+    private RecruitmentStatus status;
+
 }
