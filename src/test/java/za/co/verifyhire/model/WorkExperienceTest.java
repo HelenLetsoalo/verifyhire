712f13f3c2f1493564f571a626d61e8ee5e8d877
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 08:53:32 +0200

    feat: Add company name getter test

--- src/test/java/za/co/verifyhire/model/WorkExperienceTest.java
+++ src/test/java/za/co/verifyhire/model/WorkExperienceTest.java
@@ -3,15 +3,33 @@ package za.co.verifyhire.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Test
-public void shouldReturnExperienceId() {
-    WorkExperience experience = new WorkExperience(
-            "W1",
-            "ABC Company",
-            "Software Developer",
-            LocalDate.of(2025, 1, 1),
-            LocalDate.of(2025, 6, 30)
-    );
 
-    assertEquals("W1", experience.getExperienceId());
+public class WorkExperienceTest {
+    @Test
+    public void shouldReturnExperienceId() {
+        WorkExperience experience = new WorkExperience(
+                "W1",
+                "ABC Company",
+                "Software Developer",
+                LocalDate.of(2025, 1, 1),
+                LocalDate.of(2025, 6, 30)
+        );
+
+        assertEquals("W1", experience.getExperienceId());
+    }
+
+    @Test
+    public void shouldReturnCompanyName() {
+        WorkExperience experience = new WorkExperience(
+                "W1",
+                "ABC Company",
+                "Software Developer",
+                LocalDate.of(2025, 1, 1),
+                LocalDate.of(2025, 6, 30)
+        );
+
+        assertEquals("ABC Company", experience.getCompanyName());
+    }
+
+
 }
