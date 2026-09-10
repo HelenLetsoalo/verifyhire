f20e03bf7b82bfdf6c7fe70c13d758262c92a0b7
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 20:55:50 +0200

    feat: Add organisation constructor

--- src/main/java/za/co/verifyhire/model/Organisation.java
+++ src/main/java/za/co/verifyhire/model/Organisation.java
@@ -9,5 +9,13 @@ public class Organisation {
     private String services;
     private String phone;
 
+    public Organisation(String organisationId,String name,String location,String email,String services,String phone) {
+        this.organisationId = organisationId;
+        this.name = name;
+        this.location = location;
+        this.email = email;
+        this.services = services;
+        this.phone = phone;
+    }
 
 }
