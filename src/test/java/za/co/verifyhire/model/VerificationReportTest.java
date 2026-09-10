e191da38faad1df1c98a66e0278507cc70c2e6e8
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:37:14 +0200

    feat: Add verification report ID getter test

--- src/test/java/za/co/verifyhire/model/VerificationReportTest.java
+++ src/test/java/za/co/verifyhire/model/VerificationReportTest.java
@@ -3,4 +3,19 @@ package za.co.verifyhire.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class VerificationReportTest {}
+public class VerificationReportTest {
+
+    @Test
+    public void shouldReturnReportId() {
+        VerificationReport report = new VerificationReport(
+                "VR1",
+                null,
+                true,
+                "All requirements verified."
+        );
+
+        assertEquals("VR1", report.getReportId());
+    }
+
+
+}
