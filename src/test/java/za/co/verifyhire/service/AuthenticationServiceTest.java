42650ec2dce6848f8facc612f2cda9f80b53a4db
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 23:15:11 +0200

    feat: Add user registration test to AuthenticationServiceTest

--- src/test/java/za/co/verifyhire/service/AuthenticationServiceTest.java
+++ src/test/java/za/co/verifyhire/service/AuthenticationServiceTest.java
@@ -1,6 +1,26 @@
-package za.co.verifyhire.model;
+package za.co.verifyhire.service;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import za.co.verifyhire.model.User;
 
-public class AuthenticationServiceTest {}
+public class AuthenticationServiceTest {
+
+    @Test
+    public void shouldRegisterUser() {
+        AuthenticationService service = new AuthenticationService();
+
+        User user = new User(
+                "U1",
+                "Helen",
+                "Letsoalo",
+                "helen@example.com",
+                "password",
+                "Johannesburg"
+        );
+
+        service.registerUser(user);
+
+        assertEquals(user, service.findByEmail("helen@example.com"));
+    }
+}
