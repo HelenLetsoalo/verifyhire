bf5f7b53fda505a26e31be5445271ffd8a797c88
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 09:57:47 +0200

    fix: Add recruiter company name getter test

--- src/test/java/za/co/verifyhire/model/RecruiterTest.java
+++ src/test/java/za/co/verifyhire/model/RecruiterTest.java
@@ -81,6 +81,35 @@ public class RecruiterTest {
         assertEquals("helen@123.com", recruiter.getEmail());
     }
 
+    @Test
+    public void shouldReturnLocation() {
+        Recruiter recruiter = new Recruiter(
+                "1",
+                "Helen",
+                "Letsoalo",
+                "helen@123.com",
+                "password",
+                "Johannesburg",
+                "ABC Company"
+        );
+
+        assertEquals("Johannesburg", recruiter.getLocation());
+    }
+
+    @Test
+    public void shouldReturnCompanyName() {
+        Recruiter recruiter = new Recruiter(
+                "1",
+                "Helen",
+                "Letsoalo",
+                "helen@123.com",
+                "password",
+                "Johannesburg",
+                "ABC Company"
+        );
+
+        assertEquals("ABC Company", recruiter.getCompanyName());
+    }
 }
 
 
