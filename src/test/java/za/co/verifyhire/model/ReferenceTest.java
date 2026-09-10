4944f261df1f16ad16e5a023ead1b8a9f0fc75e1
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 09:43:50 +0200

    feat: Add reference name getter test

--- src/test/java/za/co/verifyhire/model/ReferenceTest.java
+++ src/test/java/za/co/verifyhire/model/ReferenceTest.java
@@ -4,15 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Test
-public void shouldReturnReferenceId() {
-    Reference reference = new Reference(
-            "R1",
-            "John Smith",
-            "Manager",
-            "john@example.com",
-            "0111234567"
-    );
+public class ReferenceTest {
+    @Test
+    public void shouldReturnReferenceId() {
+        Reference reference = new Reference(
+                "R1",
+                "John Smith",
+                "Manager",
+                "john@example.com",
+                "0111234567"
+        );
 
-    assertEquals("R1", reference.getReferenceId());
+        assertEquals("R1", reference.getReferenceId());
+    }
+
+    @Test
+    public void shouldReturnName() {
+        Reference reference = new Reference(
+                "R1",
+                "John Smith",
+                "Manager",
+                "john@example.com",
+                "0111234567"
+        );
+
+        assertEquals("John Smith", reference.getName());
+    }
 }
+
