25bd9453c48d9e3d3966ab3baf3e5a560dd9b337
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 09:54:08 +0200

    fix: remove duplicate test

--- src/test/java/za/co/verifyhire/model/ReferenceTest.java
+++ src/test/java/za/co/verifyhire/model/ReferenceTest.java
@@ -57,17 +57,5 @@ public class ReferenceTest {
         assertEquals("john@example.com", reference.getEmail());
     }
 
-    @Test
-    public void shouldReturnEmail() {
-        Reference reference = new Reference(
-                "R1",
-                "John Smith",
-                "Manager",
-                "john@example.com",
-                "0111234567"
-        );
-
-        assertEquals("john@example.com", reference.getEmail());
-    }
 }
 
