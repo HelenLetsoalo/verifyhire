c099f400e06d457f29c5040c7d7d1c6ab5874a15
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:55:00 +0200

    feat: add private attributes

--- src/main/java/za/co/verifyhire/model/VerificationReport.java
+++ src/main/java/za/co/verifyhire/model/VerificationReport.java
@@ -2,4 +2,8 @@ package za.co.verifyhire.model;
 
 public class VerificationReport {
 
+    private String reportId;
+    private JobApplication application;
+    private VerificationResult result;
+    private String reason;
 }
