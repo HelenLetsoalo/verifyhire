8ec99a078a29d449d08962954b2ca9e4b2361a66
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-11 00:27:51 +0200

    test: create JobServiceTest class

---
+++ src/test/java/za/co/verifyhire/service/JobServiceTest.java
@@ -0,0 +1,7 @@
+package za.co.verifyhire.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import za.co.verifyhire.model.Job;
+
+public class JobServiceTest {}
