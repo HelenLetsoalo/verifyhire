ceddbd9284bb810e25c11fac8092d66248ed44af
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:14:44 +0200

    feat: Add document ID getter test

--- src/test/java/za/co/verifyhire/model/DocumentTest.java
+++ src/test/java/za/co/verifyhire/model/DocumentTest.java
@@ -6,5 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DocumentTest {
 
+    @Test
+    public void shouldReturnDocumentId() {
+        Document document = new Document(
+                "D1",
+                null,
+                "Qualification Certificate",
+                "degree.pdf"
+        );
 
+        assertEquals("D1", document.getDocumentId());
+    }
 }
