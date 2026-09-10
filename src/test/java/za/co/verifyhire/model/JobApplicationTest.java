4ed6a21d8d863269173d66ab75ca5106ce1af02e
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 11:06:14 +0200

    feat: Add application test getter test

--- src/test/java/za/co/verifyhire/model/JobApplicationTest.java
+++ src/test/java/za/co/verifyhire/model/JobApplicationTest.java
@@ -2,6 +2,7 @@ package za.co.verifyhire.model;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
 
 public class JobApplicationTest {
     @Test
@@ -44,5 +45,25 @@ public class JobApplicationTest {
         assertEquals(candidate, application.getCandidate());
     }
 
-        
+    @Test
+    public void shouldReturnJob() {
+        Candidate candidate = new Candidate(
+                "C1", "Helen", "Letsoalo",
+                "helen@123.com", "password", "Johannesburg"
+        );
+
+        Job job = new Job(
+                "J1",
+                "Software Developer",
+                "Develop software",
+                "Johannesburg",
+                "Java, Git, Maven"
+        );
+
+        JobApplication application = new JobApplication("A1", candidate, job);
+
+        assertEquals(job, application.getJob());
+    }
+
+
 }
