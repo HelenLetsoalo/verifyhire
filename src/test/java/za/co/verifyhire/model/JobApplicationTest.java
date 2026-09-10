d371eab09add37e87c79c7eb573240e6ef00f04a
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 10:59:23 +0200

    feat: Add application ID getter test

---
+++ src/test/java/za/co/verifyhire/model/JobApplicationTest.java
@@ -0,0 +1,26 @@
+package za.co.verifyhire.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JobApplicationTest {
+    @Test
+    public void shouldReturnApplicationId() {
+        Candidate candidate = new Candidate(
+                "C1", "Helen", "Letsoalo",
+                "helen@123.com", "password", "Johannesburg"
+        );
+
+        Job job = new Job(
+                "J1",
+                "Software Developer",
+                "Develop software",
+                "Johannesburg",
+                "Java, Git, Maven"
+        );
+
+        JobApplication application = new JobApplication("A1", candidate, job);
+
+        assertEquals("A1", application.getApplicationId());
+    }
+}
