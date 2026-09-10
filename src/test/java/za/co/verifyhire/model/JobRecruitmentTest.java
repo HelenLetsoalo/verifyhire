5e97df69064dc2b2dcbffbb07328cd0484d0184e
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 21:44:47 +0200

    feat: Add recruitment ID getter test

--- src/test/java/za/co/verifyhire/model/JobRecruitmentTest.java
+++ src/test/java/za/co/verifyhire/model/JobRecruitmentTest.java
@@ -3,4 +3,16 @@ package za.co.verifyhire.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class JobRecruitmentTest {}
+public class JobRecruitmentTest {
+
+    @Test
+    public void shouldReturnRecruitmentId() {
+        JobRecruitment recruitment = new JobRecruitment(
+                "JR1",
+                null,
+                null
+        );
+
+        assertEquals("JR1", recruitment.getRecruitmentId());
+    }
+}
