1f238238623991a4a89100fdb6479b2f5bac40eb
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-10 09:21:25 +0200

    feat: import LocalDate

--- src/test/java/za/co/verifyhire/model/WorkExperienceTest.java
+++ src/test/java/za/co/verifyhire/model/WorkExperienceTest.java
@@ -2,7 +2,7 @@ package za.co.verifyhire.model;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.time.LocalDate;
 
 public class WorkExperienceTest {
     @Test
