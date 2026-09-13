f8fdd821c9dc55803b5b283b14b89da4163a4e06
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-13 17:53:54 +0200

    test:add package and imports

---
+++ src/main/java/za/co/verifyhire/service/ApplicationService.java
@@ -0,0 +1,9 @@
+package za.co.verifyhire.service;
+
+import org.junit.jupiter.api.Test;
+
+import za.co.verifyhire.model.Candidate;
+import za.co.verifyhire.model.Job;
+import za.co.verifyhire.model.JobApplication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
