532d366270a5c4a0efa8be331be94042bffa846e
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-14 14:35:35 +0200

    feat: create ApplicationService class

--- src/main/java/za/co/verifyhire/service/ApplicationService.java
+++ src/main/java/za/co/verifyhire/service/ApplicationService.java
@@ -1,5 +1,11 @@
 package za.co.verifyhire.service;
 
+import za.co.verifyhire.model.Candidate;
+import za.co.verifyhire.model.Job;
+import za.co.verifyhire.model.JobApplication;
+
 public class ApplicationService {
+
+
 
 }
