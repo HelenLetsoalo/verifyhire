ec50abc85fb3cf14c3f97cf91dae6e0278d2396f
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-15 21:49:32 +0200

    feat: add users database table

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    user_id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    location VARCHAR(150) NOT NULL
+);
+
