e38462781cd563119dbb6541be1b7453a4c2bf1a
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-18 00:06:54 +0200

    feat: add candidate setup to application entry point

---
+++ src/main/java/za/co/verifyhire/Main.java
@@ -0,0 +1,20 @@
+package za.co.verifyhire;
+
+import za.co.verifyhire.model.Candidate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Candidate candidate = new Candidate(
+                "C1",
+                "Helen",
+                "Letsoalo",
+                "helen@example.com",
+                "password",
+                "Johannesburg"
+        );
+
+    }
+}
+
