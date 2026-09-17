3125626394654554aa64bf86d0c10ba79e1dc0e1
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-18 00:12:54 +0200

    feat: create application in application entry point

--- src/main/java/za/co/verifyhire/Main.java
+++ src/main/java/za/co/verifyhire/Main.java
@@ -2,6 +2,8 @@ package za.co.verifyhire;
 
 import za.co.verifyhire.model.Candidate;
 import za.co.verifyhire.model.Job;
+import za.co.verifyhire.model.JobApplication;
+import za.co.verifyhire.service.ApplicationService;
 
 public class Main {
 
@@ -22,6 +24,14 @@ public class Main {
                 "Develop and maintain software.",
                 "Johannesburg",
                 "Java"
+        );
+
+        ApplicationService applicationService = new ApplicationService();
+
+        JobApplication application = applicationService.createApplication(
+                "A1",
+                candidate,
+                job
         );
 
     }
