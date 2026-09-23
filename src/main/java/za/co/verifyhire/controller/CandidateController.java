32e01b26d53d0569d556e75b616a716d441f30cf
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-23 16:50:32 +0200

    feat: add CandidateController with temporary mock data

--- src/main/java/za/co/verifyhire/controller/CandidateController.java
+++ src/main/java/za/co/verifyhire/controller/CandidateController.java
@@ -1,4 +1,15 @@
 package za.co.verifyhire.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CandidateController {
+
+    @GetMapping("/candidates")
+    public List<String> getCandidates() {
+        return List.of("U001");
+    }
 }
