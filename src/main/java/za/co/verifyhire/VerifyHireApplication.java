63a1e3806e638140c55e124ecd54e69b7805bd7a
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-23 16:37:48 +0200

    feat: initialize Spring Boot entry point

--- src/main/java/za/co/verifyhire/VerifyHireApplication.java
+++ src/main/java/za/co/verifyhire/VerifyHireApplication.java
@@ -1,4 +1,12 @@
 package za.co.verifyhire;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class VerifyHireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerifyHireApplication.class, args);
+    }
 }
