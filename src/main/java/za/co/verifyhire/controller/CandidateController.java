72a6457d34b544d82ce0dbf23d0b1cb87b6b7b7b
Helen Letsoalo <heletjhb025@student.wethinkcode.co.za>
2026-09-23 17:23:37 +0200

    feat: add constructor injection for CandidateRepository in controller

--- src/main/java/za/co/verifyhire/controller/CandidateController.java
+++ src/main/java/za/co/verifyhire/controller/CandidateController.java
@@ -2,14 +2,21 @@ package za.co.verifyhire.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.verifyhire.database.CandidateRepository;
 
 import java.util.List;
 
 @RestController
 public class CandidateController {
 
+    private final CandidateRepository candidateRepository;
+
+    public CandidateController(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
     @GetMapping("/candidates")
-    public List<String> getCandidates() {
-        return List.of("U001");
+    public List<String> getCandidates() throws Exception {
+        return candidateRepository.findAllCandidates();
     }
 }
